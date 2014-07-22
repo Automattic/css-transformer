7c16e4385809de2b27a7c157f57a4ded2afec006
Damian Suarez <rdsuarez@gmail.com>
2014-07-22 20:55:38 -0300

    properties: export css transform properties array

---
+++ properties.js
@@ -0,0 +1,28 @@
+
+/**
+ * Export css transform properties
+ * https://developer.mozilla.org/es/docs/Web/CSS/transform
+ */
+
+module.exports = [
+  'matrix',
+  'translate',
+  'translateX',
+  'translateY',
+  'scale',
+  'scaleX',
+  'scaleY',
+  'rotate',
+  'skewX',
+  'skewY',
+  'matrix',
+  'translate',
+  'translateZ',
+  'scale',
+  'scaleZ',
+  'rotate',
+  'rotateX',
+  'rotateY',
+  'rotateZ',
+  'perspective',
+];
